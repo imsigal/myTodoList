0ee9510b005172860f1c1970bdfd52d22b2ea773
sigal <ssamburski@gmail.com>
2020-01-13 22:30:19 +0200

    fix bug when fill the checkbox with correct status

--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -5,21 +5,24 @@ import './ListItemComponent.css';
 export default class ListItemComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isSelected:false
+    
+          this.state = {
+            wasChanged:false
         };
+
     }
+  
+
     handleCheckBoxChange = (event) => {
             console.log(event.target);
-            if (event.target.checked) {
-                this.props.item.isCompleted = true;
+            if (event.target.checked!=this.props.item.isCompleted)
+            {
+                this.props.item.isCompleted=event.target.checked;
+                this.setState({
+                    wasChanged:true
+                    });
             }
-            else {
-                this.props.item.isCompleted = false;
-            }
-            this.setState({
-                isSelected:event.target.checked
-            });
+
         };
         render()
         {
@@ -31,13 +34,14 @@ export default class ListItemComponent extends Component {
             else {
                 completedClass = "regular-text";
             }
+
             return (
             <div className="form-check">
                 <label className={completedClass}>
                     <input
                         type="checkbox"
                         name={item.id}
-                        checked={this.state.isSelected}
+                        checked={item.isCompleted}
                         onChange={this.handleCheckBoxChange}
                         className="form-check-input"
                     />
